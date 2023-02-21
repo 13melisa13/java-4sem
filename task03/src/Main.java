957a84fd3bc2dfadb691cdd45fc9ac95b463de63
13melisa13 <114099749+13melisa13@users.noreply.github.com>
2023-02-21 14:17:12 +0300

    fix

--- task03/src/Main.java
+++ task03/src/Main.java
@@ -8,43 +8,51 @@ public class Main {
         ArrayList<Integer> unsafeList = new ArrayList<>(1000);
         HashSet<Integer> unsafeSet = new HashSet<>(1000);
 
-        new Thread(() -> {
+        Thread one = new Thread(() -> {
             for (int i = 0; i < 499; i++) {
                 list.add(i);
                 set.add(i);
                 unsafeList.add(i);
                 unsafeSet.add(i);
             }
-        }).start();
+        });
 
-        new Thread(() -> {
+        Thread second = new Thread(() -> {
             for (int i = 499; i < 1000; i++) {
                 list.add(i);
                 set.add(i);
                 unsafeList.add(i);
                 unsafeSet.add(i);
             }
-        }).start();
-
+        });
+        one.start();
+        second.start();
         try {
-            Thread.sleep(30);
+            Thread.sleep(3);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
         System.out.println("SynchronizedList size = " + list.size());
-        for (Integer integer : list) {
-            System.out.print(integer + " ");
-        }
-        System.out.println();
+//        for (Integer integer : list) {
+//            System.out.print(integer + " ");
+//        }
+//        System.out.println();
 
         System.out.println("SemaphoreSet size = " + set.size());
-        for (Integer integer : set) {
-            System.out.print(integer + " ");
-        }
-        System.out.println();
+//        for (Integer integer : set) {
+//            System.out.print(integer + " ");
+//        }
+//        System.out.println();
 
         System.out.println("ArrayList size = " + unsafeList.size());
+//        for (Integer integer : unsafeList) {
+//            System.out.print(integer + " ");
+//        }
+//        System.out.println();
         System.out.println("HashSet size = " + unsafeSet.size());
+//        for (Integer integer : unsafeSet) {
+//            System.out.print(integer + " ");
+//        }
     }
 }

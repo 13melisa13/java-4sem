89f8e22a55cb3854d4253f828e81c441cc6f6402
13melisa13 <114099749+13melisa13@users.noreply.github.com>
2023-04-04 15:39:18 +0300

    12-13

---
+++ task13_spring/src/main/java/com/example/task13_spring/Task13SpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.task13_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task13SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task13SpringApplication.class, args);
+    }
+
+}
---
+++ task13_spring/src/test/java/com/example/task13_spring/Task13SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task13_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task13SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

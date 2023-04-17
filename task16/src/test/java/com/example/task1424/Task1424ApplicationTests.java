54be72513c3b4da96ebd92e733afe7aa8c982c3b
13melisa13 <114099749+13melisa13@users.noreply.github.com>
2023-04-17 23:30:06 +0300

    15-16

---
+++ task16/src/test/java/com/example/task1424/Task1424ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task1424;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1424ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
